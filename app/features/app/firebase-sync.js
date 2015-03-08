260ec60f65de4fa2bbbcf8da1a15f922f29ef5ee
marcopeg <marco.pegoraro@gmail.com>
2015-03-08 17:00:27 +0100

    detect connection loss

--- app/features/app/firebase-sync.js
+++ app/features/app/firebase-sync.js
@@ -4,11 +4,25 @@ var Firebase = require('firebase');
 module.exports = {
     init: function() {
         var store = this.store;
-        var currentSlide = new Firebase('https://fireslide.firebaseio.com/currentSlide');
+
+        var db = new Firebase('https://fireslide.firebaseio.com');
+        var currentSlide = db.child('currentSlide');
+        var firstVal = false;
         
+        // sync firebase -> app-store
         currentSlide.on('value', function(snapshot) {
             store.trigger('set-slide', snapshot.val());
             store.trigger('sync-status', true);
+            firstVal = true;
+        });
+
+        // keep the connection status update after the first value has 
+        // been downloaded into the data store
+        db.child('.info/connected').on('value', function(connectedSnap) {
+            if (!firstVal) {
+                return;
+            }
+            store.trigger('sync-status', connectedSnap.val() === true);    
         });
 
         // remote behaviour: update back from state to remote sync point
