d79553d3574c1996b18ea5ed7e0223ca916b535e
marcopeg <marco.pegoraro@gmail.com>
2015-03-08 13:20:24 +0100

    improved jsx

--- app/features/app/app.jsx
+++ app/features/app/app.jsx
@@ -24,31 +24,19 @@ module.exports = React.createClass({
         var isPreloading = dataIsReady && this.state.cached < this.state.slides.length;
         var isShowtime = dataIsReady && !isPreloading;
 
-        // split UI in many empty pieces that can be conditionally populated
-        var preload = null;
-        var slideshow = null;
-
         // are we preloading?
         if (isPreloading) {
-            preload = <Preload total={this.state.slides.length} cached={this.state.cached} />;
+            return <Preload total={this.state.slides.length} cached={this.state.cached} />;
         }
 
         // is it show time?
         if (isShowtime) {
-            slideshow = React.createElement(SlideDeck, {
-                slides: this.state.slides,
-                current: this.state.current
-            });
+            return <SlideDeck slides={this.state.slides} current={this.state.current} />;
         }
 
         // the main UI schema is a composition of high-level interface modules
         // which receive properties from the central state.
-        return (
-            <div>
-                {preload}
-                {slideshow}
-            </div>
-        );
+        return null;
     }
 
 });
